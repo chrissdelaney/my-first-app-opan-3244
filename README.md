25b8f3d4b16464cb2818a4efaeb27c3c74e49107
Chris Delaney <cjdelaney02@gmail.com>
2023-10-30 17:48:17 -0400

    unemployment.py added

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ conda create -n activate my-first-env python=3.11
 conda activate my-first-env
 ```
 
+install packages:
+```sh
+pip install -r requirements.txt
+```
+
 # USAGE
 ```sh
 python -m app.my_script.py
---
+++ app/unemployment.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import plotly.express
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+print(API_KEY)
+
+quit()
+
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+
+response = requests.get(request_url)
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response))
+print(parsed_response.keys())
+
+data = parsed_response["data"]
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date?
+# Display the unemployment rate using a percent sign.
+
+print("-------------------------")
+print("LATEST UNEMPLOYMENT RATE:")
+#print(data[0])
+print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+
+# Challenge B
+#
+# What is the average unemployment rate for all months during this calendar year?
+# ... How many months does this cover?
+
+from statistics import mean
+
+this_year = [d for d in data if "2023-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{round(mean(rates_this_year), 2)}%")
+print("NO MONTHS:", len(this_year))
+
+# Challenge C
+#
+# Plot a line chart of unemployment rates over time.
+
+dates = [d["date"] for d in data]
+rates = [float(d["value"]) for d in data]
+
+fig = plotly.line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+fig.show()
