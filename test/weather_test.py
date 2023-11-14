2f995e81b260ec56748ff9eb0e1ae329a2121232
Chris Delaney <cjdelaney02@gmail.com>
2023-11-13 23:28:28 -0500

    turning weather_test into a function

--- test/weather_test.py
+++ test/weather_test.py
@@ -3,17 +3,18 @@ import pandas as pd
 from app.weather import get_location_data, get_current_weather_data, display_weather_forecast
 
 
-#test for getting location data
-assert isinstance(get_location_data("20007"), pd.Series) 
-assert isinstance(get_location_data("08540"), pd.Series) 
+def test_weather():
+    #test for getting location data
+    assert isinstance(get_location_data("20007"), pd.Series) 
+    assert isinstance(get_location_data("08540"), pd.Series) 
 
-#test for getting current weather data
-assert get_current_weather_data(latitude=38.9144, longitude=-77.074, postal_code="20007").status_code == 200
+    #test for getting current weather data
+    assert get_current_weather_data(latitude=38.9144, longitude=-77.074, postal_code="20007").status_code == 200
 
-#test for weather forecast
-r = get_current_weather_data(latitude=38.9144, longitude=-77.074, postal_code="20007")
-assert display_weather_forecast(r) == 7
+    #test for weather forecast
+    r = get_current_weather_data(latitude=38.9144, longitude=-77.074, postal_code="20007")
+    assert display_weather_forecast(r) == 7
 
-print()
-print("ALL TESTS PASS!")
+    print()
+    print("ALL TESTS PASS!")
 
